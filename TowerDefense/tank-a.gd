059433a9a4def9fc4ebe760dac5bc1226b2a6e33
oxben <oxben@free.fr>
2015-03-20 22:32:15 +0100

    Add tanks' wreck on top of DetailsTileMap

--- TowerDefense/tank-a.gd
+++ TowerDefense/tank-a.gd
@@ -71,7 +71,7 @@ func hit(damage, continuous=false):
 		wreck.set_frame(randi() % wreck.get_hframes())
 		var root = get_node("/root")
 		global.current_level.add_child(wreck)
-		global.current_level.move_child(wreck, 0)
+		global.current_level.move_child(wreck, 2)
 		
 		var texture = ImageTexture.new()
 		texture.load("res://assets/images/tank-a-dead.png")
