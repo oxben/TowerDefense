e7f2d271f55653a79b9b48e59e57325dae3c67ca
oxben <oxben@free.fr>
2018-02-20 18:58:56 +0100

    Tweak ascending label

--- TowerDefense/ascending-label.gd
+++ TowerDefense/ascending-label.gd
@@ -1,19 +1,14 @@
 
 extends Label
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
-
 var speed = 40
-var max_move = 20
+var max_move = 40
 var move = 0
 
 func _ready():
-	# Initalization here
-	set_process(true)
+	set_physics_process(true)
 
-func _process(delta):
+func _physics_process(delta):
 	var pos = get_position()
 	var delta_move= (speed * delta)
 	pos.y -= delta_move
